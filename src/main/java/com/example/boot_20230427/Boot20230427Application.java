bba2754179ec1401c0c764ce8dfdc0f4f7364f2b
LDJ <ldj8196@naver.com>
2023-05-02 15:28:47 +0900

    first commit

---
+++ src/main/java/com/example/boot_20230427/Boot20230427Application.java
@@ -0,0 +1,22 @@
+package com.example.boot_20230427;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+
+// 새로운 패키지를 생성하기 역할을 부여하면 반드시 실행파일에 등록해야함
+// classpath ==> resources와 동일함.
+@SpringBootApplication
+@PropertySource(value = {"classpath:global.properties"}) // 직접만든 환경설정 파일 위치
+@MapperScan(basePackages = {"com.example.boot_20230427.mapper"}) // 맵퍼 위치 설정
+@ComponentScan(basePackages = {"com.example.boot_20230427.controller","com.example.boot_20230427.service"}) // 컨트롤러 위치 및 서비스 설정
+public class Boot20230427Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot20230427Application.class, args);         
+	}
+
+	
+}
