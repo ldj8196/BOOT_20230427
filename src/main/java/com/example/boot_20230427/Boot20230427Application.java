54a6ffbcc9a91fb7d0cd6d6fdca9973eb58d3d43
LDJ <ldj8196@naver.com>
2023-05-02 16:40:52 +0900

    image

--- src/main/java/com/example/boot_20230427/Boot20230427Application.java
+++ src/main/java/com/example/boot_20230427/Boot20230427Application.java
@@ -11,12 +11,10 @@ import org.springframework.context.annotation.PropertySource;
 @SpringBootApplication
 @PropertySource(value = {"classpath:global.properties"}) // 직접만든 환경설정 파일 위치
 @MapperScan(basePackages = {"com.example.boot_20230427.mapper"}) // 맵퍼 위치 설정
-@ComponentScan(basePackages = {"com.example.boot_20230427.controller","com.example.boot_20230427.service"}) // 컨트롤러 위치 및 서비스 설정
+@ComponentScan(basePackages = {"com.example.boot_20230427.controller","com.example.boot_20230427.service","com.example.boot_20230427.config"}) // 컨트롤러, 서비스 위치, 시큐리티 환경설정 
 public class Boot20230427Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Boot20230427Application.class, args);         
 	}
-
-	
 }
