276dd41c480434345eebb60adf981a63ca2a07cd
LDJ <ldj8196@naver.com>
2023-05-25 09:19:14 +0900

    war

--- src/main/java/com/example/boot_20230427/Boot20230427Application.java
+++ src/main/java/com/example/boot_20230427/Boot20230427Application.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
@@ -30,7 +31,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 							   "com.example.boot_20230427.aop"}) // 컨트롤러, 서비스 위치, 시큐리티 환경설정 
 @EntityScan(basePackages = {"com.example.boot_20230427.entity"}) // 엔티티 위치
 @EnableJpaRepositories(basePackages = {"com.example.boot_20230427.repository"}) // 저장소 위치
-public class Boot20230427Application {
+public class Boot20230427Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Boot20230427Application.class, args);         
